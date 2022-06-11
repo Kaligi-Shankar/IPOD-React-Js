dbdd99946c5270c12f1821f8f1bee8fce478bbdd
Kaligi-Shankar <skaligi007@gmail.com>
2022-06-11 16:14:03 +0530

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,7 @@
+/---------- To Run the APP -------/
+
+$ npm Start
+
+/------------Before Running the App -----/
+
+install Node modules
---
+++ src/SongsObj.js
@@ -0,0 +1,29 @@
+const songs = [
+  {
+    name: "Song 1",
+    link: "./audio.mp3",
+  },
+  {
+    name: "Song 2",
+     link: "./audio.mp3",
+  },
+  {
+    name: "Song 3",
+     link: "./audio.mp3",
+  },
+  {
+    name: "Song 4",
+     link: "./audio.mp3",
+  },
+  {
+    name: "Song 5",
+     link: "./audio.mp3",
+  },
+  {
+    name: "Song 6",
+     link: "./audio.mp3",
+  },
+];
+
+
+export default songs
